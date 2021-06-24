5bfda18179021eb261f70a30e012859e2f686e34
Silas Henrique <silash35@gmail.com>
2021-06-24 17:06:45 -0300

    remove dividers

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,54 +1,47 @@
-import Divider from "../divider";
 import styles from "./services.module.scss";
 
 export default function Services() {
   return (
-    <>
-      <Divider top />
-
-      <article id="services" className={styles.services}>
-        <section>
-          <h2>Serviços Oferecidos</h2>
-        </section>
-        <section className={styles.cards}>
-          <article>
-            <h3>Informática</h3>
-            <p>
-              conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
-              aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
-              palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
-              sumário etc.); redação original de qualquer obra escrita.conjunto das palavras
-              escritas, em livro, folheto, documento etc. ( a comentários, aditamentos, sumário
-              etc.); redação original de qualquer obra escrita.( a comentários, aditamentos, sumário
-              etc.); redação original de qualquer obra escrita.
-            </p>
-          </article>
-          <article>
-            <h3>Informática</h3>
-            <p>
-              conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
-              aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
-              palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
-              sumário etc.); redação original de qualquer obra escrita.conjunto das palavras
-              escritas, em livro, folheto, documento etc. ( a comentários, aditamentos, sumário
-              etc.); redação original de qualquer obra escrita.
-            </p>
-          </article>
-          <article>
-            <h3>Informática</h3>
-            <p>
-              conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
-              aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
-              palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
-              sumário etc.); redação original de qualquer obra escrita.conjunto das palavras
-              escritas, em livro, folheto, documento etc. ( a comentários, aditamentos, sumário
-              etc.); redação original de qualquer obra escrita.
-            </p>
-          </article>
-        </section>
-      </article>
-
-      <Divider />
-    </>
+    <article id="services" className={styles.services}>
+      <section>
+        <h2>Serviços Oferecidos</h2>
+      </section>
+      <section className={styles.cards}>
+        <article>
+          <h3>Informática</h3>
+          <p>
+            conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
+            aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
+            palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
+            sumário etc.); redação original de qualquer obra escrita.conjunto das palavras escritas,
+            em livro, folheto, documento etc. ( a comentários, aditamentos, sumário etc.); redação
+            original de qualquer obra escrita.( a comentários, aditamentos, sumário etc.); redação
+            original de qualquer obra escrita.
+          </p>
+        </article>
+        <article>
+          <h3>Informática</h3>
+          <p>
+            conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
+            aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
+            palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
+            sumário etc.); redação original de qualquer obra escrita.conjunto das palavras escritas,
+            em livro, folheto, documento etc. ( a comentários, aditamentos, sumário etc.); redação
+            original de qualquer obra escrita.
+          </p>
+        </article>
+        <article>
+          <h3>Informática</h3>
+          <p>
+            conjunto das palavras escritas, em livro, folheto, documento etc. ( a comentários,
+            aditamentos, sumário etc.); redação original de qualquer obra escrita.conjunto das
+            palavras escritas, em livro, folheto, documento etc. ( a comentários, aditamentos,
+            sumário etc.); redação original de qualquer obra escrita.conjunto das palavras escritas,
+            em livro, folheto, documento etc. ( a comentários, aditamentos, sumário etc.); redação
+            original de qualquer obra escrita.
+          </p>
+        </article>
+      </section>
+    </article>
   );
 }
