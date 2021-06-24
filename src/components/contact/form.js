43e5327c4fb7bc048f14ff0209dfe5846e9ed037
Silas Henrique <silash35@gmail.com>
2021-06-24 18:35:23 -0300

    add form button

--- src/components/contact/form.js
+++ src/components/contact/form.js
@@ -6,11 +6,11 @@ export default function Form() {
       <div>
         <div className={styles.textField}>
           <label htmlFor="name">Seu nome:</label>
-          <input name="name" />
+          <input name="name" required />
         </div>
         <div className={styles.textField}>
           <label htmlFor="mail">Seu Email:</label>
-          <input type="mail" name="mail" />
+          <input type="email" name="mail" required />
         </div>
       </div>
       <div className={styles.textField}>
@@ -18,7 +18,10 @@ export default function Form() {
         <textarea aria-label="Insira sua mensagem" name="message" />
       </div>
 
-      <button type="submit">Enviar</button>
+      <button type="submit" className={styles.send}>
+        <img src="/resources/icons/send.svg" alt="paper plane icon" />
+        Enviar
+      </button>
     </form>
   );
 }
