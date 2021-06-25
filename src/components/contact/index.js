f86da17f9a03ae2ba011b0db7d8d4a5ab73e626d
Silas Henrique <silash35@gmail.com>
2021-06-25 13:59:50 -0300

    fix list of contact information

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -16,12 +16,7 @@ export default function Contact() {
                 contato@makilab.com.br
               </a>
             </li>
-            <li>
-              <img src="/resources/icons/whatsapp.svg" alt="Whatsapp Icon" />
-              <a href="tel:+5571985447786" title="Phone number">
-                +55 71 98544-7786
-              </a>
-            </li>
+
             <li>
               <img src="/resources/icons/instagram.svg" alt="instagram Icon" />
               <a
@@ -31,6 +26,30 @@ export default function Contact() {
                 rel="noopener noreferrer"
               >
                 @makilabservicos
+              </a>
+            </li>
+            <li>
+              <img src="/resources/icons/phone.svg" alt="Phone Icon" />
+              <a href="tel:+5571985447786" title="Phone number">
+                +55 71 98544-7786
+              </a>
+            </li>
+            <li>
+              <img src="/resources/icons/phone.svg" alt="Phone Icon" />
+              <a href="tel:+5571999646282" title="Phone number">
+                +55 71 99964-6282
+              </a>
+            </li>
+            <li>
+              <img src="/resources/icons/phone.svg" alt="Phone Icon" />
+              <a href="tel:+557121320220" title="Phone number">
+                +55 71 2132-0220
+              </a>
+            </li>
+            <li className={styles.whatsapp}>
+              <a href="https://api.whatsapp.com/send?phone=5571985447786">
+                <img src="/resources/icons/whatsapp.svg" alt="Whatsapp Icon" />
+                Whatsapp
               </a>
             </li>
           </ul>
