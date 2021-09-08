abcab85016b895f5ed03d101f98e9c2a2b024408
Silas Henrique <silash35@gmail.com>
2021-09-07 22:26:57 -0300

    fix header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,27 @@
+import Link from "next/link";
+
 import styles from "./header.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <nav>
-        <a href="/" className={styles.image}>
-          <img alt="Ícone do MakiLab; O desenho de uma Coruja" src="/text.png" height="40"></img>
-        </a>
+        <Link as="/" href="/">
+          <a className={styles.image}>
+            <img alt="Ícone do MakiLab; O desenho de uma Coruja" src="/text.png" height="40"></img>
+          </a>
+        </Link>
 
         <div>
-          <a className={styles.headerButton} href="#services">
-            Serviços
-          </a>
-          <a className={styles.headerButton} href="#partners">
-            Parceiros
-          </a>
-          <a className={`${styles.headerButton} ${styles.contact}`} href="#contact">
-            Contato
-          </a>
+          <Link as="/#services" href="/#services">
+            <a className={styles.headerButton}>Serviços</a>
+          </Link>
+          <Link as="/#partners" href="/#partners">
+            <a className={styles.headerButton}>Parceiros</a>
+          </Link>
+          <Link as="/#contact" href="/#contact">
+            <a className={`${styles.headerButton} ${styles.contact}`}>Contato</a>
+          </Link>
         </div>
       </nav>
     </header>
