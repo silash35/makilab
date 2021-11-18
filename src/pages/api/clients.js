b741507f831c1f72dec1483bbd8303155c62c790
Silas Henrique <silash35@gmail.com>
2021-11-18 11:50:20 -0300

    add client api (with out database)

---
+++ src/pages/api/clients.js
@@ -0,0 +1,25 @@
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+
+export default async function clients(req, res) {
+  try {
+    const cookies = cookie.parse(req.headers.cookie);
+    const cookieName = "authorization";
+    console.log(cookies?.[cookieName]);
+    jwt.verify(cookies?.[cookieName], process.env.PASSWORD);
+  } catch (err) {
+    res.statusCode = 401;
+    res.end(
+      JSON.stringify({
+        body: { text: "Deu errado vc não tá logado" },
+      })
+    );
+  }
+
+  res.statusCode = 200;
+  res.end(
+    JSON.stringify({
+      body: { text: "Tudo certo parabens" },
+    })
+  );
+}
