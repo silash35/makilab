ca34e4f9cdd6251c3ef393051a1d257fc4dc400b
Silas Henrique <silash35@gmail.com>
2022-01-15 21:38:33 -0300

    remove button css and now using MUI on adminMenu

--- src/components/adminMenu/index.js
+++ src/components/adminMenu/index.js
@@ -1,3 +1,4 @@
+import Button from "@mui/material/Button";
 import Link from "next/link";
 
 import styles from "./adminMenu.module.scss";
@@ -5,20 +6,72 @@ import styles from "./adminMenu.module.scss";
 export default function AdminMenu() {
   return (
     <div className={styles.adminMenu}>
-      <Link as="admin/newOS" href="admin/newOS">
-        <a className={styles.red}>Criar nova Ordem de Serviço</a>
-      </Link>
-      <Link as="admin/editOSs" href="admin/editOSs">
-        <a className={styles.yellow}>Gerenciar Ordens de Serviço</a>
-      </Link>
-      <Link as="admin/newClient" href="admin/newClient">
-        <a className={styles.cyan}>Adicionar Cliente</a>
-      </Link>
+      <BigButton link="admin/newOS">Criar nova Ordem de Serviço</BigButton>
 
-      <button disabled className={styles.disabled}>
+      <BigButton
+        link="admin/editOSs"
+        style={{
+          backgroundColor: "#ff9926",
+          "&:focus": {
+            backgroundColor: "#ffba6b",
+          },
+          "&:hover": {
+            backgroundColor: "#ff8800",
+          },
+        }}
+      >
+        Gerenciar Ordens de Serviço
+      </BigButton>
+
+      <BigButton
+        link="admin/newClient"
+        style={{
+          backgroundColor: "#00e4c5",
+          "&:focus": {
+            backgroundColor: "#41ffe6",
+          },
+          "&:hover": {
+            backgroundColor: "#1daa97",
+          },
+        }}
+      >
+        Adicionar Cliente
+      </BigButton>
+
+      <BigButton disabled component={undefined}>
         Gerenciar Clientes <br />
         (Em breve)
-      </button>
+      </BigButton>
     </div>
+  );
+}
+
+function BigButton({ children, style, disabled, component = "a", link }) {
+  const sx = {
+    width: "250px",
+    height: "180px",
+    padding: "16px",
+    margin: "40px",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "larger",
+    borderRadius: "16px",
+    textTransform: "none",
+
+    ...style,
+  };
+
+  const button = (
+    <Button variant="contained" component={component} sx={sx} disabled={disabled}>
+      {children}
+    </Button>
+  );
+
+  return disabled ? (
+    button
+  ) : (
+    <Link as={link} href={link}>
+      {button}
+    </Link>
   );
 }
