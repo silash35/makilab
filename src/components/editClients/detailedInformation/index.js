07a6bdbf8156f0eb3cf0abcbedfd0471d9b28db6
Silas Henrique <silash35@gmail.com>
2022-01-23 18:24:40 -0300

    add equipments cards

--- src/components/editClients/detailedInformation/index.js
+++ src/components/editClients/detailedInformation/index.js
@@ -16,7 +16,6 @@ export default function DetailedInformation({ client, reload }) {
     <Box className={styles.detailedInformation}>
       <h2>Informações Detalhadas</h2>
 
-      <h3>Sobre o Cliente</h3>
       <p>{client.name && `Nome: ${client.name}`}</p>
       <p>{client.email && `Email: ${client.email}`}</p>
       <p>{client.address && `Endereço: ${client.address}`}</p>
@@ -26,43 +25,49 @@ export default function DetailedInformation({ client, reload }) {
       <p>{client.cpfOrCnpj && `CPF ou CNPJ: ${client.cpfOrCnpj}`}</p>
 
       <Box className={styles.cardsContainer}>
-        {/*
-        <Card variant="outlined">
-          <CardContent>
-            <h3>Sobre o Equipamento</h3>
-            <p>{equipment.batchOrImei && `Lote ou IMEI: ${equipment.batchOrImei}`}</p>
-            <p>{equipment.equipment_number && `Numero de Serie: ${equipment.equipment_number}`}</p>
-            <p>
-              {equipment.isUnderWarranty
-                ? "Equipamento em Garantia"
-                : "Equipamento Fora de Garantia"}
-            </p>
-            <p>
-              {(() => {
-                if (equipment.isUnderWarranty === false) {
-                  if (equipment.isBudgetApproved === null) {
-                    return "Orçamento ainda não aprovado";
-                  } else if (equipment.isBudgetApproved === false) {
-                    return "Orçamento Negado";
-                  } else if (equipment.isBudgetApproved === true) {
-                    return "Orçamento Aprovado";
-                  }
-                }
-              })()}
-            </p>
-            <p>{equipment.accessories && `Acessórios: ${equipment.accessories}`}</p>
-            <p>
-              {equipment.equipmentCondition &&
-                `Condição do equipamento: ${equipment.equipmentCondition}`}
-            </p>
-            <p>
-              {equipment.problemDescription &&
-                `Descrição do problema: ${equipment.problemDescription}`}
-            </p>
-            {equipment.wasEdited && <Alert severity="info">Esse equipamento já foi editado</Alert>}
-          </CardContent>
-        </Card>
-              */}
+        {client.equipment.map((equipment) => {
+          return (
+            <Card variant="outlined" key={equipment.id}>
+              <CardContent>
+                <h3>Sobre o Equipamento</h3>
+                <p>{equipment.batchOrImei && `Lote ou IMEI: ${equipment.batchOrImei}`}</p>
+                <p>
+                  {equipment.equipment_number && `Numero de Serie: ${equipment.equipment_number}`}
+                </p>
+                <p>
+                  {equipment.isUnderWarranty
+                    ? "Equipamento em Garantia"
+                    : "Equipamento Fora de Garantia"}
+                </p>
+                <p>
+                  {(() => {
+                    if (equipment.isUnderWarranty === false) {
+                      if (equipment.isBudgetApproved === null) {
+                        return "Orçamento ainda não aprovado";
+                      } else if (equipment.isBudgetApproved === false) {
+                        return "Orçamento Negado";
+                      } else if (equipment.isBudgetApproved === true) {
+                        return "Orçamento Aprovado";
+                      }
+                    }
+                  })()}
+                </p>
+                <p>{equipment.accessories && `Acessórios: ${equipment.accessories}`}</p>
+                <p>
+                  {equipment.equipmentCondition &&
+                    `Condição do equipamento: ${equipment.equipmentCondition}`}
+                </p>
+                <p>
+                  {equipment.problemDescription &&
+                    `Descrição do problema: ${equipment.problemDescription}`}
+                </p>
+                {equipment.wasEdited && (
+                  <Alert severity="info">Esse equipamento já foi editado</Alert>
+                )}
+              </CardContent>
+            </Card>
+          );
+        })}
       </Box>
 
       <div className={styles.flex}>
