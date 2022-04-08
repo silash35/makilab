4300e2e77e3569ada2c1bfb78fd73844444ba1ad
Silas Henrique <silash35@gmail.com>
2022-04-08 11:23:00 -0300

    update manage-offline readme

--- packages/manager-offline/update.py
+++ packages/manager-offline/update.py
@@ -11,19 +11,13 @@ dirpath = path.join("OpenSOM-linux-x64")
 if path.exists(dirpath) and path.isdir(dirpath):
     shutil.rmtree(dirpath)
 
-dirpath = path.join(serverPath, "node_modules")
-if path.exists(dirpath) and path.isdir(dirpath):
-    shutil.rmtree(dirpath)
-
-dirpath = path.join(rootPath, "node_modules")
+dirpath = path.join("node_modules")
 if path.exists(dirpath) and path.isdir(dirpath):
     shutil.rmtree(dirpath)
 
 
 print("Downloading and installing new files")
 system("git pull")
-system("pnpm install")
-chdir(serverPath)
 system("pnpm install")
 
 print("Building new server files")
