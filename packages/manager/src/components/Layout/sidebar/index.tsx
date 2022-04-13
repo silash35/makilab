61a4e763fe5eeb21faf4280c025828569e385603
Silas Henrique <silash35@gmail.com>
2022-04-13 16:17:03 -0300

    add Mobile Drawer

--- packages/manager/src/components/Layout/sidebar/index.tsx
+++ packages/manager/src/components/Layout/sidebar/index.tsx
@@ -2,22 +2,41 @@ import AddOSIcon from "@mui/icons-material/Add";
 import HomeIcon from "@mui/icons-material/Home";
 import EditClientsIcon from "@mui/icons-material/ManageAccounts";
 import EditOSsIcon from "@mui/icons-material/ManageSearch";
+import MenuIcon from "@mui/icons-material/Menu";
 import AddClientIcon from "@mui/icons-material/PersonAdd";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 import styles from "./sidebar.module.scss";
 
 export default function Sidebar() {
+  const matches = useMediaQuery("(max-width: 1023px)");
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <aside className={styles.aside}>
-      <Drawer className={styles.drawer} variant="permanent" anchor="left">
+      {matches && (
+        <div className={styles.menuButton}>
+          <IconButton aria-label="Open Menu" onClick={() => setIsOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+        </div>
+      )}
+      <Drawer
+        className={styles.drawer}
+        variant={matches ? "temporary" : "permanent"}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
         <Link href="/">
           <a className={styles.image}>
             <img alt="OpenSOM, with SOM written in green" src="/text.svg" height="40" />
