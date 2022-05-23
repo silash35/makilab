262f1f900a52f5a736cbaf47818815a578fd58de
Silas Henrique <silash35@gmail.com>
2022-05-23 19:24:56 -0300

    add custom error page

---
+++ packages/frontend/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import Stack from "@mui/material/Stack";
+
+export default function Custom404() {
+  return (
+    <Stack height="100%" justifyContent="center" alignItems="center">
+      <h1>404 - Page Not Found</h1>
+    </Stack>
+  );
+}
