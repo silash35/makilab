7c09e00700c7fc785bcc169f0d5bae1f4c5d8451
Silas Henrique <silash35@gmail.com>
2022-05-24 16:43:15 -0300

    give up

--- packages/backend2/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-
-@Controller()
-export class AppController {
-  @Get()
-  home(): string {
-    return "This is a backend of OpenSOM. Where the magic happens.";
-  }
-}
