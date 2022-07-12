1504eaa77590657ae426185a4d16c6a6500504b3
Silas Henrique <silash35@gmail.com>
2022-07-12 15:59:15 -0300

    remove builds checks on Heroku

--- packages/tracker/next.config.mjs
+++ packages/tracker/next.config.mjs
@@ -1,11 +1,29 @@
-export default {
-  reactStrictMode: true,
-  env: {
-    SITE_URL: process.env.SITE_URL,
-    API_URL: process.env.API_URL,
-  },
-  i18n: {
-    locales: ["pt", "en"],
-    defaultLocale: "pt",
-  },
+export default () => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      SITE_URL: process.env.SITE_URL,
+      API_URL: process.env.API_URL,
+    },
+    i18n: {
+      locales: ["pt", "en"],
+      defaultLocale: "pt",
+    },
+  };
+
+  if (process.env.PROCFILE === "packages/tracker/Procfile") {
+    // If is running on Heroku, there is no need to check eslint or typescript
+    // The checks was already done in the github actions
+    nextConfig.eslint = {
+      ignoreDuringBuilds: true,
+    };
+    nextConfig.typescript = {
+      ignoreBuildErrors: true,
+    };
+  }
+
+  return nextConfig;
 };
