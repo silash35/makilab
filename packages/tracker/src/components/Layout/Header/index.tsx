b9a1b1997fb07621348d98201bb14725fdf788cc
Silas Henrique <silash35@gmail.com>
2023-02-18 17:01:27 -0300

    update tracker

--- packages/tracker/src/components/Layout/Header/index.tsx
+++ packages/tracker/src/components/Layout/Header/index.tsx
@@ -14,21 +14,25 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <nav>
-        <Link href="/">
-          <a className={styles.image}>
-            <img alt={`${COMPANY.name} logo`} src="/YOUR_COMPANY_LOGO.svg" height="40" />
-          </a>
+        <Link href="/" className={styles.image}>
+          <img alt={`${COMPANY.name} logo`} src="/YOUR_COMPANY_LOGO.svg" height="40" />
         </Link>
 
         <div>
-          <Link href={`mailto:${COMPANY.email}`}>
-            <a className={styles.headerButton}>E-mail</a>
+          <Link href={`mailto:${COMPANY.email}`} className={styles.headerButton}>
+            E-mail
           </Link>
-          <Link href={`tel:${COMPANY.phones[0].replace(/[^0-9]/g, "")}`}>
-            <a className={styles.headerButton}>{t.phone}</a>
+          <Link
+            href={`tel:${COMPANY.phones[0].replace(/[^0-9]/g, "")}`}
+            className={styles.headerButton}
+          >
+            {t.phone}
           </Link>
-          <Link href={`https://wa.me/${COMPANY.whatsapp.replace(/[^0-9]/g, "")}`}>
-            <a className={`${styles.headerButton} ${styles.contact}`}>WhatsApp</a>
+          <Link
+            href={`https://wa.me/${COMPANY.whatsapp.replace(/[^0-9]/g, "")}`}
+            className={`${styles.headerButton} ${styles.contact}`}
+          >
+            WhatsApp
           </Link>
         </div>
       </nav>
